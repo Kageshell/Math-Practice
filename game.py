d3c098e137e612524d0ea7c57bce5fde9763ca4b
Kageshell <hackerpatatalol22@gmail.com>
2026-04-09 22:31:59 +0200

    Added all options, code shorter and better responses after anwsering

--- game.py
+++ game.py
@@ -6,7 +6,11 @@ def choix():
         num1 = random.randint(1,100)
         num2 = random.randint(1,100)
         r = input(f'{num1} + {num2} = ')
-        print(int(r) == (num1 + num2))
+        if int(r) == (num1 + num2):
+            print("Bonne réponse !")
+        else:
+            print("Faux, le résultat était", num1+num2)
+
 
     if c == 1:
         num1 = random.randint(1,100)
@@ -14,21 +18,32 @@ def choix():
         mini = min(num1,num2)
         maxi = max(num1,num2)
         r = input(f'{maxi} - {mini} = ')
-        print(float(r) == (maxi - mini))
+        if int(r) == (maxi - mini):
+            print("Bonne réponse !")
+        else:
+            print("Faux, le résultat était", maxi-mini)
 
     if c == 2:
         num1 = random.randint(1,20)
         num2 = random.randint(1,9)
         r = input(f'{num1} x {num2} = ')
-        print(int(r) == (num1*num2))
+        if int(r) == (num1*num2):
+            print('Bonne réponse !')
+
+        else:
+            print("Faux, le résultat était", num1*num2)
 
     if c == 3:
-        num2 = random.randint(1,9)
+        num2 = random.randint(2, 9)
+        num1 = random.randrange(num2, 999, num2)
 
-        if num2 == 2:
-            num1 = random.randrange(2,999,2)
+        r = input(f'{num1} : {num2} = ')
+        if int(r) == (num1//num2):
+            print("Bonne réponse !")
 
-   choix()
+        else:
+            print("Faux, le résultat était", num1//num2)
 
-choix()
+while True:
+    choix()
 
